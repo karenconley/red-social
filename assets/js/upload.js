fdd01136e5a74d56990fe693072f3cf71013bf45
karenconley <karen.c.echeverria@gmail.com>
2018-01-05 00:43:05 -0300

    post funcional y sección vet

--- assets/js/upload.js
+++ assets/js/upload.js
@@ -39,40 +39,91 @@
 
 */
 
+//subir POST SOS y comentar dentro de la página
 $(document).ready(function () {
-    var num = 0;
-
-    //event click btn addSOS
+    $('#postSOS').hide();
+    //event click btn publicar SOS post
     $('#add').click(function () {
         var title = $('#titleSOS').val();
         var description = $('#description').val();
         var comuna = $("#comuna option:selected").text();
-        //var comuna = $('#comuna').val();
-        //var contactId = 'contact_' + num;
-        //var deleteId = 'delete_' + num;
-
-        //add SOS
-        $('#postSOS').append(
-            '<p class="nameStyle">' + title + description + comuna + '</p>' 
+        var dt = new Date();
+        var time = dt.getHours() + ":" + dt.getMinutes();
+        var date = dt.toLocaleDateString();
+        var image = new Image();
+        var src = 'assets/img/intro.jpg';
+        //muestra contenedor de post publicado
+        $('#postSOS').show();
+        //agrega los datos
+        $('#title').append(
+            '<h3 class="titleC">' +
+            title +
+            '</h3>'
         );
-        
-        //remove addSOS form
-        $('.container').remove();
-
-        //sum cont Id
-        num++;
-
-        //input clean
-        $('#title').val("");
-        $('#description').val("");
-        $('#comuna').val("");
-
-
-        /*
-        $('#' + deleteId).click(function () {
-            $('#' + contactId).remove();
+        $('#comunaText').text(comuna);
+        $('#descriptionText').text(description);
+        $('#timeText').text(time + '  ' + date);
+        $('#imgSOSpreview').append(
+            '<img src="' +
+            src +
+            '" alt="" class="img-responsive imgSOS">'
+        );
+        //remueve contenedor formulario
+        $('#uploadCont').remove();
+        //funcion para publicar comentarios
+        $('#sendComment').click(function () {
+            var comment = $('#comment').val();
+            $('#commentsCont').append(
+                '<hr>' +
+                '<div class="media">' +
+                '<div class="media-left">' +
+                '<a href="javascript:void(0)">' +
+                '<img src="https://bootdey.com/img/Content/avatar/avatar1.png" alt="" class="media-object img-circle avatarPost">' +
+                '</a>' +
+                '</div>' +
+                '<div class="media-body">' +
+                '<h4 class="media-heading"> Lucky Sans' +
+                '<br>' +
+                '<small>' +
+                '<i class="fa fa-clock-o">' +
+                '</i>' +
+                date +
+                ','+
+                ' ' +
+                time +
+                ' hrs' +
+                '</small>' +
+                '</h4>' +
+                '<p>' + comment +
+                '</p>' +
+                '<ul class="nav nav-pills pull-left">' +
+                '<li>' +
+                '<a href="" title="">' +
+                '<i class="glyphicon glyphicon-thumbs-up">' +
+                '</i>' +
+                ' 8' +
+                '</a>' +
+                '</li>' +
+                '<li>' +
+                '<a href="" title="">' +
+                '<i class=" glyphicon glyphicon-comment">' +
+                '</i>' +
+                ' 8' +
+                '</a>' +
+                '</li>' +
+                '<li>' +
+                '<a href="" title="">' +
+                '<i class="glyphicon glyphicon-share-alt">' +
+                '</i>' +
+                ' 10' +
+                '</a>' +
+                '</li>' +
+                '</ul>' +
+                '</div>' +
+                '</div>'
+            );
+            //limpia input
+            $('#comment').val("");
         });
-        */
     });
-
 });
